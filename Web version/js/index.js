899916c9d7a2d76ab6ca71f7af8604a6ce9bffca
Samuel Matheson <samuelmatheson15@gmail.com>
2019-11-12 23:56:40 -0500

    Fixed up login screen

--- Web version/js/index.js
+++ Web version/js/index.js
@@ -1,3 +1,17 @@
 window.addEventListener('load',()=>{
     document.getElementById('email_put').focus();
+    login.initalize();
 });
+
+let login = {
+    initalize:function(){
+        document.getElementById('login_btn').addEventListener('click',()=>{
+            document.getElementById('login_btn').className="swapbtn_active";
+            document.getElementById('signup_btn').className="swapbtn";
+        });
+        document.getElementById('signup_btn').addEventListener('click',()=>{
+            document.getElementById('login_btn').className="swapbtn";
+            document.getElementById('signup_btn').className="swapbtn_active";
+        });
+    },
+}
