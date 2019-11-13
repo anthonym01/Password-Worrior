b08bb6be1e40e842969138fda2bafcd7c3f7da5e
Samuel Matheson <samuelmatheson15@gmail.com>
2019-11-13 00:14:25 -0500

    Animated the first load of teh page

--- Web version/js/index.js
+++ Web version/js/index.js
@@ -1,7 +1,18 @@
 window.addEventListener('load',()=>{
     document.getElementById('email_put').focus();
     login.initalize();
+
+
+    close_loading();
 });
+
+function close_loading(){
+    document.getElementById('page_shade').style.backgroundColor="rgba(0,0,0,0)";
+    setTimeout(()=>{
+        document.getElementById('login_screen').style.transform="translate(0,105vh)";
+        document.getElementById('page_shade').style.display="none";
+    },1000);
+}
 
 let login = {
     initalize:function(){
